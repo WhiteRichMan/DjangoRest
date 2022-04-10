0a7a18ebbebf67e5d910a687f9935477657605ba
whiterichman <karpikrus404@gmail.com>
2022-04-10 21:21:18 +0600

    First settings for DjangoRest

---
+++ apps/auths/views.py
@@ -0,0 +1,28 @@
+from rest_framework import permissions
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.request import Request
+
+# from django.db.models import QuerySet
+
+from auths.models import CustomUser
+
+
+class ListUsers(APIView):
+    """
+    View for CustomUser.
+    * Does-not require token authentication.
+    * Only superusers are able to access this view.
+    """
+    permission_classes: tuple = (
+        permissions.IsAdminUser,
+    )
+
+    def get(self, request: Request) -> Response:
+        """Return list of all users."""
+
+        data: list = [
+            user.password for user in CustomUser.objects.all()
+        ]
+        return Response(data)
---
+++ settings/urls.py
@@ -0,0 +1,26 @@
+from rest_framework.routers import DefaultRouter
+
+from django.contrib import admin
+from django.conf import settings
+from django.urls import (
+    path,
+    include,
+)
+from django.conf.urls.static import static
+
+
+urlpatterns = [
+    path(settings.ADMIN_SITE_URL, admin.site.urls),
+    path('', include('auths.urls')),
+] + static(
+    settings.STATIC_URL,
+    document_root=settings.STATIC_ROOT
+) + static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT
+)
+if settings.DEBUG:
+    urlpatterns += [
+        path('__debug__/', include('debug_toolbar.urls')),
+    ]
+
